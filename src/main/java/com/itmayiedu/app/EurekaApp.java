d75a46a10b720a216277a7f59b52ad26d68e967a
caohu <362193486@qq.com>
2020-11-23 14:29:43 +0800

    ceshi

--- src/main/java/com/itmayiedu/app/EurekaApp.java
+++ src/main/java/com/itmayiedu/app/EurekaApp.java
@@ -10,7 +10,7 @@ public class EurekaApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApp.class, args);
-		System.out.print("AAA");
+		System.out.print("AAABBB");
 	}
 
 }
